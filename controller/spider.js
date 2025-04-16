a5d02455d1b377d16c7ee1f283e1eb3a8d06c572
liny61836@gmail.com <liny61836@gmail.com>
2025-04-16 17:31:33 +0800

    修改蜘蛛访问排序逻辑

--- controller/spider.js
+++ controller/spider.js
@@ -1,28 +1,40 @@
-const { Op } = require("sequelize");
+const { Op, literal } = require("sequelize");
 const SpiderModel = require("../model/spider");
 
 module.exports = {
   async getSpider(req, res, next) {
     try {
-      const { domain, date, type, page = 1, page_size = 10 } = req.query;
+      const { domain, date, type, page = 1, page_size = 10, by_count } = req.query;
       const where = {};
+
       if (domain) {
         where.domain = {
           [Op.like]: `%${domain}%`
         };
       }
-      if (type){
-        where.type = type
+
+      if (type) {
+        where.type = type;
       }
+
       if (date) {
         where.created_time = {
           [Op.gte]: `${date} 00:00:00`,
           [Op.lt]: `${date} 23:59:59`,
         };
       }
+
+      // 排序逻辑
+      let order = [["created_time", "DESC"]];
+      if (by_count === "asc") {
+        order = [[literal("CAST(count AS UNSIGNED)"), "ASC"]];
+      } else if (by_count === "desc") {
+        order = [[literal("CAST(count AS UNSIGNED)"), "DESC"]];
+      }
+
       const { count, rows } = await SpiderModel.findAndCountAll({
         where,
-        order: [["created_time", "DESC"]],
+        order,
         limit: parseInt(page_size),
         offset: (page - 1) * page_size,
       });
