6f8f23b99fcec963999de910fc2778adf4c9abb6
liny61836@gmail.com <liny61836@gmail.com>
2025-05-07 19:03:46 +0800

    权限设置

--- router/white.js
+++ router/white.js
@@ -1,10 +1,9 @@
 const { apiPrefix } = require('../config/config.default')   // 配置文件
 // 配置白名单，不走token
-let urlList = []
+let urlList = ['/zhanqun/spider/write_log']
 let whiteList = [
     '/user/login',
-    '/user/register',
-    '/zhanqun/spider/write_log'
+    '/user/register'
 ]
 whiteList.map(item => {
     urlList.push(`${apiPrefix}${item}`)
