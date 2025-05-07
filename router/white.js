c8117b6fc2eb68bb233b468765740b08f43fb5cc
liny61836@gmail.com <liny61836@gmail.com>
2025-05-07 19:11:55 +0800

    change

--- router/white.js
+++ router/white.js
@@ -3,7 +3,8 @@ const { apiPrefix } = require('../config/config.default')   // 配置文件
 let urlList = ['/zhanqun/spider/write_log']
 let whiteList = [
     '/user/login',
-    '/user/register'
+    '/user/register',
+    '/zhanqun/spider/write_log'
 ]
 whiteList.map(item => {
     urlList.push(`${apiPrefix}${item}`)
